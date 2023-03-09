634ae463a22f3fc3b4642790dd0761f45751ca71
ychen023 <ychen023@uw.edu>
2023-03-08 22:30:56 -0800

    clean code

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Button, Card} from 'react-bootstrap';
 export default function DoraList(props) {
   let cardList = props.darayaki.map((singleData) => {
